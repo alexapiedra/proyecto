331b1729f93d6f4409a0e7b2a570fa3fc9df7946
alexandra <piedrahitagonzalez3@gmial.com>
2022-11-19 08:29:03 -0500

    insertando gato

--- proyecto1/src/App.js
+++ proyecto1/src/App.js
@@ -1,26 +1,13 @@
-// se importa libreria react
-import React from 'react';
-
-//se importa logo
 import logo from './logo.svg';
-
-//se importa estilos
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// se importa el componente padre del header
-import Headeralexa from "./components/header";
-import Footeralexa from "./components/footer";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Headeralexa></Headeralexa>
         <p>
-          Hola angel
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -30,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-        <Footeralexa></Footeralexa>
       </header>
     </div>
   );
